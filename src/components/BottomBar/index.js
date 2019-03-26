643e07a62c65c9c09353cac0076889b2ffbd9d47
arcanrun <arcan.run@gmail.com>
2019-03-26 14:13:35 +0300

    routing fix

--- src/components/BottomBar/index.js
+++ src/components/BottomBar/index.js
@@ -1,6 +1,6 @@
 //@flow
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import "./style.css";
 
@@ -8,18 +8,18 @@ import { Icon } from "../Icon";
 
 const BottomBar = () => (
   <div className="bottom-bar">
-    <Link to="/history" className="bottom-bar__item">
+    <NavLink exact to="/history" className="bottom-bar__item">
       <Icon icon="history" color="#200A9C" />
       <span>Item</span>
-    </Link>
-    <Link to="/" className="bottom-bar__item">
+    </NavLink>
+    <NavLink exact to="/" className="bottom-bar__item">
       <Icon icon="money-bag" color="#200A9C" />
       <span>Item</span>
-    </Link>
-    <Link to="/profile" className="bottom-bar__item">
+    </NavLink>
+    <NavLink exact to="/profile" className="bottom-bar__item">
       <Icon icon="profile" color="#200A9C" />
       <span>Item</span>
-    </Link>
+    </NavLink>
   </div>
 );
 
