94e681e1638ae6b10d5006c6b3527a8f02876e21
arcanrun <arcan.run@gmail.com>
2019-04-04 23:03:27 +0300

    sticky dates ...so so

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,6 +1,7 @@
 //@flow
 import React from "react";
-import style from "./History.module.css";
+import styleHistory from "./History.module.css";
+import Sticky from "react-sticky-el";
 
 import {
   OperationSign,
@@ -26,23 +27,35 @@ class History extends React.Component<PROPS, {}> {
   render() {
     const { history, isFetching } = this.props;
     const overlay = <Overlay />;
-    const verticalLine = <div className={style.verticalLine} />;
+    const verticalLine = <div className={styleHistory.verticalLine} />;
+    const a = {
+      color: "red"
+    };
     history.sort(compareDate);
 
     return isFetching ? (
       overlay
     ) : (
-      <div className={style.container}>
+      <div className={styleHistory.container}>
         {history.map((item, i) => {
           const day = Object.keys(item)[0];
 
           return (
-            <div key={i} className={style.oneDay}>
-              <div className={style.day}>{day}</div>
+            <div key={i} className={styleHistory.oneDay}>
+              <Sticky
+                boundaryElement={styleHistory.oneDay}
+                topOffset={-60}
+                stickyStyle={{
+                  marginTop: "60px"
+                }}
+              >
+                <div className={styleHistory.day}>{day}</div>
+              </Sticky>
+              {/* <div className={styleHistory.day}>{day}</div> */}
               {verticalLine}
 
               {item[day].map((elem, j) => (
-                <div key={j} className={style.operation}>
+                <div key={j} className={styleHistory.operation}>
                   <OperationType>{elem.type_cost}</OperationType>
                   <OperationSign>{elem.operation}</OperationSign>
                   <OperationValue sign={elem.operation}>
