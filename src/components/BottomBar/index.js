acc9a10bc6ce99b24cfbe08342ddb4f319a2c673
arcanrun <arcan.run@gmail.com>
2019-04-07 17:11:39 +0300

    stable version

--- src/components/BottomBar/index.js
+++ src/components/BottomBar/index.js
@@ -1,12 +1,12 @@
 //@flow
 import React from "react";
 
-import "./style.css";
+import style from "./BottomBar.module.css";
 
 import { ActiveLink } from "../index";
 
 const BottomBar = () => (
-  <div className="bottom-bar">
+  <div className={style.bottomBar}>
     <ActiveLink to="/history" icon={"history"} />
     <ActiveLink to="/" icon={"money-bag"} />
     <ActiveLink to="/profile" icon={"profile"} />
