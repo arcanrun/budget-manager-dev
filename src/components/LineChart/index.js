d3647d5e92b45005f0a970a8a43fab3d5f8b8a12
arcanrun <arcan.run@gmail.com>
2019-07-09 20:32:01 +0300

    fix outlie on dots entrance

--- src/components/LineChart/index.js
+++ src/components/LineChart/index.js
@@ -21,7 +21,6 @@ export const LineChart = ({ switcher, history }: PROPS) => {
     }
   });
   currentMonthData.reverse();
-  //   console.log(currentMonthData);
 
   var data = {
     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
