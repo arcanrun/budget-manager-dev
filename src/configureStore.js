11e1a71a5ff66a4dfceff2095de0a6412cd25c70
arcanrun <arcan.run@gmail.com>
2019-10-13 00:05:05 +0300

    logger off on production

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { logger } from "redux-logger";
 
 import { rootReducer } from "./reducers";
+const middlewares = [];
 
-const middlewares = [thunk, logger];
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+if (process.env.NODE_ENV === `development`) {
+  middlewares.push(logger);
+}
+
+middlewares.push(thunk);
+// export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = compose(applyMiddleware(...middlewares))(createStore)(
+  rootReducer
+);
