12b8305bb4e7955cfddd969a5f34af75d632aad0
arcanrun <arcan.run@gmail.com>
2019-11-19 18:52:41 +0300

    trying to setup linter

--- src/components/SettingsPage/index.js
+++ src/components/SettingsPage/index.js
@@ -1,6 +1,16 @@
 //@flow
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
 
 export const SettingsPage = () => {
-  return <div>THIS WILL BE SETTINGS PAGES</div>;
+  const [isIn, setIn] = useState(false);
+  useEffect(() => {
+    setIn(true);
+  });
+
+  return (
+    <CSSTransition in={isIn} timeout={300} classNames={"page"}>
+      <div>THIS WILL BE SETTINGS PAGES</div>
+    </CSSTransition>
+  );
 };
