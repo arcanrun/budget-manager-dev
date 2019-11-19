2f7823c515be1c27a4a4af5693d1145e6a02fd4b
arcanrun <arcan.run@gmail.com>
2019-11-19 20:11:57 +0300

    mastering theme swithcer

--- src/components/SettingsPage/index.js
+++ src/components/SettingsPage/index.js
@@ -8,9 +8,25 @@ export const SettingsPage = () => {
     setIn(true);
   });
 
+  function switchTheme() {
+    const body = document.getElementsByTagName("body")[0];
+    console.log(body.getAttribute("scheme"));
+    const scheme = body.getAttribute("scheme");
+    if (scheme === "client_dark") {
+      body.setAttribute("scheme", "client_light");
+    } else if (scheme === "client_light") {
+      body.setAttribute("scheme", "client_dark");
+    } else {
+      body.setAttribute("scheme", "client_dark");
+    }
+  }
+
   return (
     <CSSTransition in={isIn} timeout={300} classNames={"page"}>
-      <div>THIS WILL BE SETTINGS PAGES</div>
+      <>
+        <div>THIS WILL BE SETTINGS PAGES</div>
+        <button onClick={switchTheme}>switch theme</button>
+      </>
     </CSSTransition>
   );
 };
